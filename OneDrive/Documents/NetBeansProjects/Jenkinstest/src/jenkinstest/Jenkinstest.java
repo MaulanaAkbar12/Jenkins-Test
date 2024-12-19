2e8a9bf8837f8f5c597d00de40f49638c81caaee
Sulthan Fawwaz <Sulthan Fawwaz@LAPTOP-Q9IT01RK>
2024-12-19 10:30:40 +0700

    Jenkin Test

---
+++ OneDrive/Documents/NetBeansProjects/Jenkinstest/src/jenkinstest/Jenkinstest.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package jenkinstest;
+
+/**
+ *
+ * @author Sulthan Fawwaz
+ */
+public class Jenkinstest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Jenkin test semoga bisa ya allah");
+    }
+    
+}
